886369478ad9135a7222162ef4fc77fecda0dbaa
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-07 01:21:44 +0700

    Implementasi Composable WelcomeScreen sebagai halaman utama

---
+++ app/src/main/java/com/example/myquestnavigastugas/view/welcome.kt
@@ -0,0 +1,34 @@
+package com.example.myquestnavigastugas.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun WelcomeScreen(navController: NavHostController) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Selamat Datang", fontSize = 30.sp, fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.height(24.dp))
