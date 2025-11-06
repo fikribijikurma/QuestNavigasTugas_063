44725a4f01c6ada12b8b8e5e65452c5b7405dca9
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-07 01:41:37 +0700

    Definisi enum class untuk destinasi navigasi utama

---
+++ app/src/main/java/com/example/myquestnavigastugas/Navigasi.kt
@@ -0,0 +1,13 @@
+package com.example.myquestnavigastugas
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+num class PengelolaHalaman {
+    Welcome,
+    ListPeserta,
+    Formulir
+}
