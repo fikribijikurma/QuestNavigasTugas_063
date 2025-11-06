9fece69e4989ff82bb5fe84a6439f209dedd7b98
fikribijikurma <auliyafikrirahmadani@gmail.com>
2025-11-07 01:16:26 +0700

    Buat screen TampilData

---
+++ app/src/main/java/com/example/myquestnavigastugas/view/TampilData.kt
@@ -0,0 +1,41 @@
+package com.example.myquestnavigastugas.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+@Composable
+fun TampilData(navController: NavHostController) {
+    val pesertaList = stringArrayResource(id = R.array.data_peserta)
+    val gradient = Brush.verticalGradient(
+        colors = listOf(Color(0xFF313647), Color(0xFF1A3D64))
+    )
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier
+                .background(gradient)
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
